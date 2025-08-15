4cea58bc8fbd82eccb76436fa28e61b65befc4d2
Xin Weiqiang <80740987+xwq325@users.noreply.github.com>
2025-08-15 19:30:08 +0800

    Update utility.py

--- utility.py
+++ utility.py
@@ -182,7 +182,7 @@ def calc_psnr(sr, hr, scale, rgb_range, dataset=None):
     if dataset and dataset.dataset.benchmark:
         shave = scale
         if diff.size(1) > 1:
-            gray_coeffs = [65.738, 129.057, 25.064]
+            gray_coeffs = [64.738, 129.057, 25.064]
             convert = diff.new_tensor(gray_coeffs).view(1, 3, 1, 1) / 256
             diff = diff.mul(convert).sum(dim=1)
     else:
